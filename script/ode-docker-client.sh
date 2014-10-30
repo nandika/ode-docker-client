0b26e237dc44566ae1f1974c25743f0db9b97d69
sathwik <sathwik@apache.org>
2014-10-30 10:59:39 +0530

    Added Java opts environment variable, and mounted /tmp

--- script/ode-docker-client.sh
+++ script/ode-docker-client.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo $@
 
+export JAVA_OPTS="-Xmx1024M -XX:MaxPermSize=512M"
 BUILDR_ARGS="$@"
 CONTAINER_USERNAME="dummy"
 CONTAINER_GROUPNAME="dummy"
@@ -9,6 +9,8 @@ GROUP_ID=$(id -g)
 USER_ID=$( id -u)
 
 CREATE_USER_COMMAND="groupadd -f -g $GROUP_ID $CONTAINER_GROUPNAME && useradd -u $USER_ID -g $CONTAINER_GROUPNAME $CONTAINER_USERNAME && mkdir --parent $HOMEDIR && chown -R $CONTAINER_USERNAME:$CONTAINER_GROUPNAME $HOMEDIR &&"
+BUILDR_COMMAND="$CREATE_USER_COMMAND su $CONTAINER_USERNAME -c '/opt/jruby/bin/jruby -S buildr $BUILDR_ARGS'"
 
-docker run --rm -e JAVADOC=no -e JAVA_OPTS -v `pwd`:/workspace -v $HOME/.m2:/home/dummy/.m2 --entrypoint bash vanto/apache-buildr:latest-jruby-jdk7 -c "$CREATE_USER_COMMAND su $CONTAINER_USERNAME -c '/opt/jruby/bin/jruby -S buildr $BUILDR_ARGS'" 
+echo $BUILDR_COMMAND
 
+docker run --rm -e JAVADOC=no -e JAVA_OPTS -v `pwd`:/workspace -v $HOME/.m2:/home/dummy/.m2  -v /tmp:/tmp --entrypoint bash vanto/apache-buildr:latest-jruby-jdk7 -c "$BUILDR_COMMAND" 
