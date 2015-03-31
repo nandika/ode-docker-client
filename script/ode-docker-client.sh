ddd38a4429ce0fc150ccc1c4a526588fa7dd3f09
sathwik <sathwik@apache.org>
2015-03-31 20:44:24 +0530

    Changes to read local Gemfile from bundler

--- script/ode-docker-client.sh
+++ script/ode-docker-client.sh
@@ -8,9 +8,12 @@ HOMEDIR="/home/$CONTAINER_USERNAME"
 GROUP_ID=$(id -g)
 USER_ID=$( id -u)
 
-CREATE_USER_COMMAND="groupadd -f -g $GROUP_ID $CONTAINER_GROUPNAME && useradd -u $USER_ID -g $CONTAINER_GROUPNAME $CONTAINER_USERNAME && mkdir --parent $HOMEDIR && chown -R $CONTAINER_USERNAME:$CONTAINER_GROUPNAME $HOMEDIR &&"
-BUILDR_COMMAND="$CREATE_USER_COMMAND su $CONTAINER_USERNAME -c '/opt/jruby/bin/jruby -S buildr $BUILDR_ARGS'"
+CREATE_USER_COMMAND="groupadd -f -g $GROUP_ID $CONTAINER_GROUPNAME && useradd -u $USER_ID -g $CONTAINER_GROUPNAME $CONTAINER_USERNAME && mkdir --parent $HOMEDIR && chown -R $CONTAINER_USERNAME:$CONTAINER_GROUPNAME $HOMEDIR"
 
-echo $BUILDR_COMMAND
+BUNDLER_COMMAND="/opt/jruby/bin/jruby -S bundler install --gemfile=/workspace/Gemfile"
+ 
+BUILDR_COMMAND="su $CONTAINER_USERNAME -c '/opt/jruby/bin/jruby -S buildr $BUILDR_ARGS'"
 
-docker run --rm -e JAVADOC=no -e JAVA_OPTS -v `pwd`:/workspace -v $HOME/.m2:/home/dummy/.m2  -v /tmp:/tmp --entrypoint bash vanto/apache-buildr:latest-jruby-jdk7 -c "$BUILDR_COMMAND" 
+FINAL_COMMAND="$CREATE_USER_COMMAND && $BUNDLER_COMMAND && $BUILDR_COMMAND"
+
+docker run --name ODE_BUILD13x --rm -t -i -e JAVADOC=no -e JAVA_OPTS -v `pwd`:/workspace -v $HOME/.m2:/home/dummy/.m2  -v /tmp:/tmp --entrypoint bash vanto/apache-buildr:dev -c "$FINAL_COMMAND"
