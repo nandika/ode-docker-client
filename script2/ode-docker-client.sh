e43044ff75e3a23dc02373949baa05d785aab993
Nandika <nandika@apache.org>
2025-03-06 19:07:23 +0530

    script updated

--- script2/ode-docker-client.sh
+++ script2/ode-docker-client.sh
@@ -15,22 +15,25 @@
 
 #!/bin/sh
 
-docker pull vanto/apache-buildr:latest-jruby-jdk7
+docker pull jayawark/ode-buildr-jruby-java11:latest
 
 export JAVA_OPTS="-Xmx1024M -XX:MaxPermSize=512M"
 BUILDR_ARGS="$@"
 CONTAINER_USERNAME="dummy"
 CONTAINER_GROUPNAME="dummy"
 HOMEDIR="/home/$CONTAINER_USERNAME"
-GROUP_ID=$(id -g)
-USER_ID=$( id -u)
+GROUP_ID=2000
+#GROUP_ID=$(id -g)
+USER_ID=2000
+#USER_ID=$(id -g)
 
 CREATE_USER_COMMAND="groupadd -f -g $GROUP_ID $CONTAINER_GROUPNAME && useradd -u $USER_ID -g $CONTAINER_GROUPNAME $CONTAINER_USERNAME && mkdir --parent $HOMEDIR && chown -R $CONTAINER_USERNAME:$CONTAINER_GROUPNAME $HOMEDIR"
 
 BUNDLER_COMMAND="/opt/jruby/bin/jruby -S bundler install --gemfile=/workspace/Gemfile"
  
-BUILDR_COMMAND="su $CONTAINER_USERNAME -c '/opt/jruby/bin/jruby -S buildr $BUILDR_ARGS'"
+BUILDR_COMMAND="su $CONTAINER_USERNAME -c '/opt/jruby/bin/jruby -S buildr -f Rakefile $BUILDR_ARGS'"
 
 FINAL_COMMAND="$CREATE_USER_COMMAND && $BUNDLER_COMMAND && $BUILDR_COMMAND"
 
-docker run --rm -t -i -e JAVADOC=no -e JAVA_OPTS -v `pwd`:/workspace -v $HOME/.m2:/home/dummy/.m2  -v /tmp:/tmp --entrypoint bash vanto/apache-buildr:latest-jruby-jdk7 -c "$FINAL_COMMAND"
+docker run --rm -t -i -e JAVADOC=no -e JAVA_OPTS -v `pwd`:/workspace/ -v $HOME/software/repo:/home/dummy/.m2  -v /tmp:/tmp --entrypoint bash jayawark/ode-buildr-jruby-java11:latest -c "$FINAL_COMMAND"
+
